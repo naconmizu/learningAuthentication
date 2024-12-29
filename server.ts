7042b9d07041bec845531774b2b83e06a248e5a6
Nacon <mctangerina@gmail.com>
2024-12-29 11:24:02 -0300

    its not making sense anymore

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
-import { User } from "./src/components/models/user.js";
 import express from "express";
 import { Application } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { User } from "./src/components/models/user.js";
+
 // Create the express app and  import the type of app from express;
 const app: Application = express();
 
@@ -18,7 +20,7 @@ app.use(
     extended: true,
   })
 );
-// Declare The PORT Like This
+ 
 const PORT: number = 8000;
 
 app.get("/", (req, res) => {
@@ -46,10 +48,7 @@ app.post("/auth/register", async (req, res) => {
       });
        return;
     }
-
-    // ** if not create a new user ;
-    // !! Don't save the password as plain text in db . I am saving just for demonstration.
-    // ** You can use bcrypt to hash the plain password.
+ 
 
     // now create the user;
     const newUser = await User.create({
@@ -78,6 +77,83 @@ app.post("/auth/register", async (req, res) => {
 });
 
 
+// here is when i notice that i dont know what im doing 
+
+
+app.post("/auth/login", async (req, res) => {
+  try {
+    // ** Get The User Data From Body ;
+    const user = req.body;
+
+    // ** destructure the information from user;
+    const { email, password } = user;
+
+    // ** Check the (email/user) exist  in database or not ;
+    const isUserExist = await User.findOne({
+      email: email,
+    });
+
+    // ** if there is not any user we will send user not found;
+    if (!isUserExist) {
+      res.status(404).json({
+        status: 404,
+        success: false,
+        message: "User not found",
+      });
+return;
+    }
+
+    // ** if the (user) exist  in database we will check the password is valid or not ;
+    // **  compare the password in db and the password sended in the request body
+
+    const isPasswordMatched =
+      isUserExist?.password === password;
+
+    // ** if not matched send response that wrong password;
+
+    if (!isPasswordMatched) {
+      res.status(400).json({
+        status: 400,
+        success: false,
+        message: "wrong password",
+      });
+        return;
+    }
+
+    // ** if the email and password is valid create a token
+
+    /*
+    To create a token JsonWebToken (JWT) receive's 3 parameter
+    1. Payload -  This contains the claims or data you want to include in the token.
+    2. Secret Key - A secure key known only to the server used for signing the token.
+    3. expiration -  Additional settings like token expiration or algorithm selection.
+    */
+
+     
+    // ** This is our JWT Token
+    const token = jwt.sign(
+      { _id: isUserExist?._id, email: isUserExist?.email },
+      process.env.JWT_SECRET || "YOUR_SECRET",
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // send the response
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "login success",
+      token: token,
+    });
+  } catch (error: any) {
+    // Send the error message to the client
+    res.status(400).json({
+      status: 400,
+      message: error.message.toString(),
+    });
+  }
+});
 
 
 
